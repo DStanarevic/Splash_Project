a29282b2c1727a8e12d434d00ec0a8b9cf4b8344
DStanarevic <dejanstanarevic@gmai.com>
2018-05-03 15:58:56 -0400

    Import CSV

--- Splash_Project/Program.cs
+++ Splash_Project/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace Splash_Project
     {
         static void Main(string[] args)
         {
+            //Variables to hold filepath to CSV
+            string startingData;
+            string jan;
+            string feb;
+            string mar;
+
+            //Assign filepath to variables
+            startingData = @"C:\Users\Dan\Documents\Visual Studio 2015\Projects\Splash_Project\Splash_Project\CSV\StartingData.csv";
+            jan = @"C:\Users\Dan\Documents\Visual Studio 2015\Projects\Splash_Project\Splash_Project\CSV\Jan.csv";
+            feb = @"C:\Users\Dan\Documents\Visual Studio 2015\Projects\Splash_Project\Splash_Project\CSV\Feb.csv";
+            mar = @"C:\Users\Dan\Documents\Visual Studio 2015\Projects\Splash_Project\Splash_Project\CSV\Mar.csv";
+
+            //Import CSV data into Visual Studio
+            List<string> startList = File.ReadAllLines(startingData).ToList();
+            List<string> janList = File.ReadAllLines(jan).ToList();
+            List<string> febList = File.ReadAllLines(feb).ToList();
+            List<string> marList = File.ReadAllLines(mar).ToList();
         }
     }
 }
